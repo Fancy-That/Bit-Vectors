39675482325b079e28e6d307866008a0468861d5
William Santos <william.santos@hotmail.co.uk>
2018-05-14 14:46:42 +0100

    Create bit-vectors.js

---
+++ bit-vectors.js
@@ -0,0 +1,15 @@
+const bitVector=function(length){
+	this.v=new Uint8Array(length>>3);
+	this.toggleBit=(pos)=>{this.v[pos>>3]^=1<<pos%8;};
+	this.getBit=(pos)=>{return(this.v[pos>>3]&1<<pos%8)?true:false;};
+};
+
+
+const bv=new bitVector(16);
+
+for(let i=0;i<16;i++){
+	bv.toggleBit(i);
+	console.log(bv.getBit(i));
+	bv.toggleBit(i);
+	console.log(bv.getBit(i));
+}
